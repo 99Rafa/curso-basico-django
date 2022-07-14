5378e38a8d81f8ec1bd39aeb4fd076aa80b60457
Rafael Aguirre <47163455+rafael-aguirre-r@users.noreply.github.com>
2022-07-14 00:37:33 -0600

    Created question index template

--- platziawardsapp/polls/views.py
+++ platziawardsapp/polls/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from polls.models import Question
 
-# Create your views here.
+
+def index(request):
+    questions = Question.objects.all()
+    context = {
+        "latest_question_list": questions
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"This is the question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"This are the results of the question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for the question {question_id}")
