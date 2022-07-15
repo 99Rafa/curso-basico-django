6747005aa8b13745d1e5f06fbe3b68454060313a
Rafael Aguirre <47163455+rafael-aguirre-r@users.noreply.github.com>
2022-07-14 23:52:57 -0600

    Dynamic urls

--- platziawardsapp/polls/views.py
+++ platziawardsapp/polls/views.py
@@ -5,18 +5,14 @@ from polls.models import Question
 
 def index(request):
     questions = Question.objects.all()
-    context = {
-        "latest_question_list": questions
-    }
-    return render(request, 'polls/index.html', context)
+    context = {"latest_question_list": questions}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question
-    }
-    return render(request, 'polls/detail.html', context)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
