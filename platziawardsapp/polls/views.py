0fca018d1cc7530fd164cf60e458dd9aec9d6b0d
Rafael Aguirre <47163455+rafael-aguirre-r@users.noreply.github.com>
2022-07-15 00:36:40 -0600

    Changed vote view

--- platziawardsapp/polls/views.py
+++ platziawardsapp/polls/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from polls.models import Choice, Question
 
 
 def index(request):
     questions = Question.objects.all()
-    context = {"latest_question_list": questions}
-    return render(request, "polls/index.html", context)
+    return render(request, "polls/index.html", {"latest_question_list": questions})
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -20,4 +20,19 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You are voting for the question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST["choice"])
+
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "invalid choice",
+            },
+        )
