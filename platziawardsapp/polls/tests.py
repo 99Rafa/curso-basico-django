c1e9f3031bc8af577571e9943dd3f898d02808bd
Rafael Aguirre <47163455+rafael-aguirre-r@users.noreply.github.com>
2022-07-17 23:37:52 -0600

    Added tests to Question model

--- platziawardsapp/polls/tests.py
+++ platziawardsapp/polls/tests.py
@@ -7,9 +7,26 @@ from polls.models import Question
 
 
 class QuestionModelTest(TestCase):
+    def setUp(self):
+        self.question = Question("Test question?")
+
     def test_was_published_recently_with_future_question(self):
         """Was_published_recently returns False for question whose pub_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="Test question?", pub_date=time)
+        self.question.pub_date = time
 
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(self.question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """Was_published_recently returns True for question whose pub_date is a few hours old"""
+        time = timezone.now() - datetime.timedelta(hours=23)
+        self.question.pub_date = time
+
+        self.assertIs(self.question.was_published_recently(), True)
+
+    def test_was_published_recently_with_old_question(self):
+        """Was_published_recently returns False for question whose pub_date is more than 1 day old"""
+        time = timezone.now() - datetime.timedelta(days=2)
+        self.question.pub_date = time
+
+        self.assertIs(self.question.was_published_recently(), False)
