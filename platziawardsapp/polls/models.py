01d30d1317d527bff6709c4534d553bc1d51fe54
Rafael Aguirre <47163455+rafael-aguirre-r@users.noreply.github.com>
2022-07-17 23:41:17 -0600

    Removed auto_now_add from Question model

--- platziawardsapp/polls/models.py
+++ platziawardsapp/polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
